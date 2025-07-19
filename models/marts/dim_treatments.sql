e33832885356c9aba76fe5396aee41f1b9b914d7
Padma Charan <padmacharan63@gmail.com>
2025-07-19 12:51:50 +0000

    project done.

--- models/marts/dim_treatments.sql
+++ models/marts/dim_treatments.sql
@@ -13,24 +13,9 @@ with existing as (
         cost_range,
         last_updated
     from {{ ref('int_treatments') }}
-),
-final as (
-    select
-        b.surrogate_key,
-        b.treatment_id,
-        b.treatment_type,
-        b.description,
-        b.cost_range,
-        case
-            when e.treatment_id is null then current_timestamp
-            when b.description != e.description or b.cost_range != e.cost_range then current_timestamp
-            else e.last_updated
-        end as last_updated
-    from {{this}} as b
-    left join existing e on b.treatment_id = e.treatment_id
 )
-select * from final
+select * from existing
 
 {% if is_incremental() %}
-  where last_updated> (select max(last_updated) from {{ this}})
+  where last_updated> (select max(last_updated) from {{this}})
 {% endif %}
